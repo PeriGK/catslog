5c70dbcbd5bc9843746d02d7769217d7b80e4b40
Periklis <per.gkolias@gmail.com>
2018-04-22 17:05:18 +0300

    Pylinting setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ https://packaging.python.org/en/latest/distributing.html
 https://github.com/pypa/sampleproject
 """
 
-from setuptools import setup, find_packages
 from codecs import open
 from os import path
+from setuptools import setup, find_packages
 
 here = path.abspath(path.dirname(__file__))
 
@@ -17,20 +17,16 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 # Arguments marked as "Required" below must be included for upload to PyPI.
 # Fields marked as "Optional" may be commented out.
 
-# setup.py
-from setuptools import setup
-
-
 setup(
-   name="catslog",
-   version='1.0.7',
-   py_modules=['catslog'],
-   description="Logs the execution of the wrapped function and, if applicable, does a basic handling of the raised exception. Please check the github page for more detailed instructions",
-   url='https://github.com/PeriGK/catslog',
-   author='Periklis Gkolias',
-   long_description_content_type='text/markdown',
-   author_email='per.gkolias@gmail.com',
-   classifiers=[
+    name="catslog",
+    version='1.0.7',
+    py_modules=['catslog'],
+    description="Logs the execution of the wrapped function and, if applicable, does a basic handling of the raised exception. Please check the github page for more detailed instructions",
+    url='https://github.com/PeriGK/catslog',
+    author='Periklis Gkolias',
+    long_description='Please check the README file in the github repository, on the left',
+    author_email='per.gkolias@gmail.com',
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
@@ -45,9 +41,4 @@ setup(
         'Programming Language :: Python :: 3.6',],
     keywords='exceptions exception-handling logging',
     packages=find_packages(exclude=['contrib', 'docs', 'tests']),
-    project_urls={
-        'Bug Reports': 'https://github.com/PeriGK/catslog/issues',
-        'Blog': 'https://medium.com/@periklisgkolias',
-        'Source': 'https://github.com/PeriGK/catslog/',
-    }
 )
